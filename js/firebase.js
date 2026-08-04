19a3978a7d6c05c7c85dd59ba74e51d610f4583d
zaidon11 <zaidrodhanali@gmail.com>
2026-08-05 02:46:59 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,25 @@
+// ==========================================
+// AlRashed Smart V2
+// Firebase Connection
+// ==========================================
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBpt495yhuoTbjunCLIFCF_8c1NesxZHWs",
+    authDomain: "shopweb-3466b.firebaseapp.com",
+    databaseURL: "https://shopweb-3466b-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "shopweb-3466b",
+    storageBucket: "shopweb-3466b.firebasestorage.app",
+    messagingSenderId: "41811528812",
+    appId: "1:41811528812:web:618f240f8106f218a8dc49"
+};
+
+// تشغيل Firebase
+firebase.initializeApp(firebaseConfig);
+
+// الاتصال بقاعدة البيانات
+const db = firebase.database();
+
+// قاعدة البيانات الجديدة
+const DATABASE = "installments_v2";
+
+console.log("✅ AlRashed Smart V2 Connected");
